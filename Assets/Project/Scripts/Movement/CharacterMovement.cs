74310fcd58562d12a23058179b594497f351d7cb
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-05-06 18:50:58 -0600

    Update CharacterMovement.cs

--- Assets/Project/Scripts/Movement/CharacterMovement.cs
+++ Assets/Project/Scripts/Movement/CharacterMovement.cs
@@ -6,8 +6,12 @@ public abstract class CharacterMovement : MonoBehaviour
 {
     public abstract void Move(Vector2 direction);
 
-    public abstract void LeftClickDown(Vector2 position);
+    public virtual void LeftClickDown(Vector2 position)
+    {
+    }
 
-    public abstract void RightClickDown(Vector2 position);
-    
+    public virtual void RightClickDown(Vector2 position)
+    {
+    }
+
 }

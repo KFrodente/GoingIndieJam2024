ba3eb0d4b1a28e4c47e2497af478e64434e0a263
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-05-07 23:02:12 -0600

    interactables

---
+++ Assets/Project/Scripts/Interaction/ShopInteract.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopInteract : Interactable
+{
+    public override void OnInteract(BaseCharacter character)
+    {
+        //Code for Kian
+    }
+}

52ffaaccd92e1df3c686901d5249cfc6d3d2ec5e
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2024-05-10 12:20:51 -0600

    renamed circle weapon to pattern weapon

--- Assets/Project/Scripts/Weapons/PatternWeapons/PatternWeapon.cs
+++ Assets/Project/Scripts/Weapons/PatternWeapons/PatternWeapon.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleWeapon : MonoBehaviour
+public class PatternWeapon : MonoBehaviour
 {
 	enum ShootType
 	{
--- Assets/Project/Scripts/Weapons/PatternWeapons/ShapePattern.cs
+++ Assets/Project/Scripts/Weapons/PatternWeapons/ShapePattern.cs
@@ -7,12 +7,16 @@ public class ShapePattern : Pattern
 {
 	[Header("Shape")]
 	[SerializeField] Vector2[] positions;
+	[SerializeField] Vector2 scalar;
 
 	public override Vector3[] SpawnBullets(Vector3 direction)
 	{
 		Vector3[] positions = new Vector3[bulletAmount];
 
+		for (int i = 0; i < positions.Length; i++)
+		{
 
+		}
 
 		return positions;
 	}

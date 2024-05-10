3a42d67b6cfd46b927a5900de93b8451f016478b
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2024-05-10 00:36:53 -0600

    Spawning Now Has Full Functionality?
    I might be forgetting stuff

--- Assets/Project/Scripts/Weapons/PatternWeapons/CirclePattern.cs
+++ Assets/Project/Scripts/Weapons/PatternWeapons/CirclePattern.cs
@@ -7,6 +7,7 @@ public class CirclePattern : Pattern
 {
 	[Header("Circle")]
 	[SerializeField] float circleRadius;
+	[SerializeField] float arcSpawnedOn = 360;
 
 	public override Vector3[] SpawnBullets(Vector3 direction, Vector3 spawnPlacement)
 	{
@@ -14,7 +15,7 @@ public class CirclePattern : Pattern
 
 		for (int i = 0; i < bulletAmount; i++)
 		{
-			float spawnAngle = (360 / bulletAmount) * i;
+			float spawnAngle = (arcSpawnedOn / bulletAmount) * i;
 			positions[i] = (spawnPlacement + (Quaternion.Euler(0, 0, spawnAngle) * direction * circleRadius));
 			//positions.Add(Instantiate((bulletPrefab == null) ? prefab : bulletPrefab, spawnPlacement + position, Quaternion.Euler(0, 0, spawnAngle)));
 		}

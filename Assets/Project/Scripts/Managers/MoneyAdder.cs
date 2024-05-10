5f0e729dd97877d0e9355c8201f9bf5eb5c19f33
KFrodente <kianfrodente@gmail.com>
2024-05-10 17:19:26 -0600

    more money adding dev tools

--- Assets/Project/Scripts/Managers/MoneyAdder.cs
+++ Assets/Project/Scripts/Managers/MoneyAdder.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 [Tooltip("Just a test script to add money. DO NOT USE")]
 public class MoneyAdder : MonoBehaviour
 {
-    [ContextMenu(nameof(AddMoney))]
-    public void AddMoney()
+    [ContextMenu(nameof(Add10Money))]
+    public void Add10Money()
     {
         SpiritCharacter.souls += 10;
+    }
+
+    [ContextMenu(nameof(Add20Money))]
+    public void Add20Money()
+    {
+        SpiritCharacter.souls += 20;
+    }
+
+    [ContextMenu(nameof(Add30Money))]
+    public void Add30Money()
+    {
+        SpiritCharacter.souls += 30;
+    }
+
+    [ContextMenu(nameof(Add40Money))]
+    public void Add40Money()
+    {
+        SpiritCharacter.souls += 40;
+    }
+
+    [ContextMenu(nameof(Add50Money))]
+    public void Add50Money()
+    {
+        SpiritCharacter.souls += 50;
     }
 }

89243f0e37e7da2f6a5e7ceb3230dba2e89a44ea
Jacob Leonardo <leonardojacob257@gmail.com>
2024-05-10 18:06:44 -0600

    Cum

--- Assets/Project/Scripts/Souls/SpiritEssence.cs
+++ Assets/Project/Scripts/Souls/SpiritEssence.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SpiritEssence : Interactable
 {
     [SerializeField] public Essence essence;
-    public int soulCost;
+    [HideInInspector] public int soulCost;
 
     private void Awake()
     {
@@ -15,25 +15,29 @@ public class SpiritEssence : Interactable
 
     public override void OnInteract(BaseCharacter character)
     {
-        base.OnInteract(character);
-        StatEffect essenceEffect1;
-        StatEffect essenceEffect2;
-        StatEffect essenceEffect3;
-        if (essence.statType1 != StatType.None)
+        if(SpiritCharacter.souls >= soulCost)
         {
-            essenceEffect1 = new StatEffect(essence.statType1, essence.operatorType1, essence.Value1);
-            character.characterStats.AddStatModifier(essenceEffect1.GetModifier());
+            base.OnInteract(character);
+            StatEffect essenceEffect1;
+            StatEffect essenceEffect2;
+            StatEffect essenceEffect3;
+            if (essence.statType1 != StatType.None)
+            {
+                essenceEffect1 = new StatEffect(essence.statType1, essence.operatorType1, essence.Value1);
+                character.characterStats.AddStatModifier(essenceEffect1.GetModifier());
+            }
+            if (essence.statType2 != StatType.None)
+            {
+                essenceEffect2 = new StatEffect(essence.statType2, essence.operatorType2, essence.Value2);
+                character.characterStats.AddStatModifier(essenceEffect2.GetModifier());
+            }
+            if (essence.statType3 != StatType.None)
+            {
+                essenceEffect3 = new StatEffect(essence.statType3, essence.operatorType3, essence.Value3);
+                character.characterStats.AddStatModifier(essenceEffect3.GetModifier());
+            }
         }
-        if (essence.statType2 != StatType.None)
-        {
-            essenceEffect2 = new StatEffect(essence.statType2, essence.operatorType2, essence.Value2);
-            character.characterStats.AddStatModifier(essenceEffect2.GetModifier());
-        }
-        if (essence.statType3 != StatType.None)
-        {
-            essenceEffect3 = new StatEffect(essence.statType3, essence.operatorType3, essence.Value3);
-            character.characterStats.AddStatModifier(essenceEffect3.GetModifier());
-        }
+
         
     }
 }

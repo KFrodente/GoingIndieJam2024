c0a9d26f9a34b32957821a719315c562a755fb38
Jacob Leonardo <leonardojacob257@gmail.com>
2024-05-10 18:47:10 -0600

    Update SpiritEssence.cs

--- Assets/Project/Scripts/Souls/SpiritEssence.cs
+++ Assets/Project/Scripts/Souls/SpiritEssence.cs
@@ -17,25 +17,35 @@ public class SpiritEssence : Interactable
     {
         if(SpiritCharacter.souls >= soulCost)
         {
+            Debug.Log("Enough souls, starting purchase, Current souls: " + SpiritCharacter.souls);
+            SpiritCharacter.souls -= soulCost;
+            Debug.Log("Souls deducted, Current souls: " + SpiritCharacter.souls);
             base.OnInteract(character);
             StatEffect essenceEffect1;
             StatEffect essenceEffect2;
             StatEffect essenceEffect3;
             if (essence.statType1 != StatType.None)
             {
+                Debug.Log("Adding effect 1");
+                Debug.Log("Damage Before: " + character.characterStats.baseStats.Damage);
                 essenceEffect1 = new StatEffect(essence.statType1, essence.operatorType1, essence.Value1);
                 character.characterStats.AddStatModifier(essenceEffect1.GetModifier());
+                Debug.Log("Damage After: " + character.characterStats.baseStats.Damage);
             }
             if (essence.statType2 != StatType.None)
             {
+                Debug.Log("Adding effect 2");
                 essenceEffect2 = new StatEffect(essence.statType2, essence.operatorType2, essence.Value2);
                 character.characterStats.AddStatModifier(essenceEffect2.GetModifier());
             }
             if (essence.statType3 != StatType.None)
             {
+                Debug.Log("Adding effect 3");
                 essenceEffect3 = new StatEffect(essence.statType3, essence.operatorType3, essence.Value3);
                 character.characterStats.AddStatModifier(essenceEffect3.GetModifier());
             }
+
+            Destroy(gameObject);
         }
 
         

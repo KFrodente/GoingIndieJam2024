1d67050ce901e0271c5a78244e08811150e90551
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-05-16 14:36:48 -0600

    Simple Dragon Attack

--- Assets/Project/Scripts/Interaction/Obelisk.cs
+++ Assets/Project/Scripts/Interaction/Obelisk.cs
@@ -6,9 +6,18 @@ public class Obelisk : Interactable
 {
     
     [SerializeField] private PieceSeparation dragonMaker;
+    private bool activated;
     
     public override void OnInteract(BaseCharacter character)
     {
+        if(activated) return;
+        Activate();
+
+    }
+
+    private void Activate()
+    {
+        activated = true;
         dragonMaker.SpawnDragon();
     }
 }

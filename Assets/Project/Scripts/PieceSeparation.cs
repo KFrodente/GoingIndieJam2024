fe326b4df59a77d2befb968ae4bbfd283937861d
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-05-16 21:04:45 -0600

    damage

--- Assets/Project/Scripts/PieceSeparation.cs
+++ Assets/Project/Scripts/PieceSeparation.cs
@@ -63,6 +63,8 @@ public class PieceSeparation : MonoBehaviour
         segments.Remove(brokenSegment);
         brokenSegment.transform.gameObject.SetActive(false);
         Reposition();
+
+        Debug.Log("Down To: " + segments.Count);
         if (segments.Count <= 2)
         {
             DragonDamager.Die();
